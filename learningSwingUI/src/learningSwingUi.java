2d17959546316d98c899612d62b00db7dcbc4c15
ros_mmerter <ros_mmerter@WROSRM31920.seattleschools.org>
2014-10-01 08:36:58 -0700

    first commit of swing program

---
+++ learningSwingUI/src/learningSwingUi.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class learningSwingUi {
+	
+	private static void makeGui() {
+
+        JFrame frame = new JFrame("thisisaframe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //adds a label
+        JLabel label = new JLabel("This is a test");
+        frame.getContentPane().add(label);
+        
+        JButton Button = new JButton("button");
+        frame.getContentPane().add(Button);
+        
+        //display ze window
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	makeGui();
+            }
+        });
+    }
+}
+
