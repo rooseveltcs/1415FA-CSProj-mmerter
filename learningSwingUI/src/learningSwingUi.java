1d16666e24f62ef62778d1eb22ac73b0399eca1c
ros_mmerter <ros_mmerter@WROSRM31920.seattleschools.org>
2014-10-03 08:32:35 -0700

    commit, little work was done

--- learningSwingUI/src/learningSwingUi.java
+++ learningSwingUI/src/learningSwingUi.java
@@ -1,30 +1,29 @@
-import javax.swing.*;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class learningSwingUi {
-	
-	private static void makeGui() {
 
-        JFrame frame = new JFrame("thisisaframe");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //adds a label
-        JLabel label = new JLabel("This is a test");
-        frame.getContentPane().add(label);
-        
-        JButton Button = new JButton("button");
-        frame.getContentPane().add(Button);
-        
-        //display ze window
-        frame.pack();
-        frame.setVisible(true);
-    }
- 
-    public static void main(String[] args) {
-    //    javax.swing.SwingUtilities.invokeLater(new Runnable() {
-           // public void run() {
-            	makeGui();
-         //   }
-      //  });
-    }
+	public learningSwingUi() {
+		JFrame frame = new JFrame("first Ui");
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel(new BorderLayout());
+		//panel.setLayout
+		
+		
+		
+		JButton button = new JButton("button");
+		panel.add(button);
+
+		frame.add(panel, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new learningSwingUi();
+	}
 }
-
